d9e278169cec6d53f39c83466507197348ded819
placidowang <placidow@gmail.com>
2020-03-24 11:34:21 -0500

    added to PAJoiner controller

--- backend/app/controllers/part_attachment_joiners_controller.rb
+++ backend/app/controllers/part_attachment_joiners_controller.rb
@@ -1,2 +1,23 @@
 class PartAttachmentJoinersController < ApplicationController
+  def create
+    PartAttachmentJoiner.create(part_attachment_params)
+  end
+
+  def edit
+  end
+
+  def update
+    part_attachment = PartAttachmentJoiner.find(params[:id])
+    part_attachment.update(part_attachment_params)
+  end
+
+  def destroy
+    part_attachment = PartAttachmentJoiner.find(params[:id])
+    part_attachment.destroy
+  end
+
+  private
+  def part_attachment_params
+    params.require(:part_attachment_joiner).permit(:part_id, :attachment_id)
+  end
 end
